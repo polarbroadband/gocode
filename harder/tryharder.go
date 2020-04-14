2c9335635c4e74e1f421b08f46b2dcda1d9f0183
Kelvin Wang <kelvin.wang@telus.com>
2020-04-14 12:52:20 -0400

    add four lines

--- harder/tryharder.go
+++ harder/tryharder.go
@@ -8,4 +8,8 @@ func TryHarder() {
 	fmt.Println("Try Harder++ primary change")
 	fmt.Println("Try Harder++ more line++ 01")
 	fmt.Println("Try Harder++ more line++ 02")
+	fmt.Println("Try Harder++ more line++ 03")
+	fmt.Println("Try Harder++ more line++ 04")
+	fmt.Println("Try Harder++ more line++ 05")
+	fmt.Println("Try Harder++ more line++ 06")
 }
